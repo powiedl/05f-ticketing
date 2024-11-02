c579c004b03d04337d8d956cb04e6a0e073a2619
powiedl <159603185+powiedl@users.noreply.github.com>
2024-11-02 14:39:16 +0100

    192-Require Auth for Route Access - Auth Service finished

--- auth/src/errors/index.ts
+++ auth/src/errors/index.ts
@@ -2,3 +2,4 @@ export { RequestValidationError } from './request-validation-errors';
 export { DatabaseConnectionError } from './database-connection-error';
 export { NotFoundError } from './not-found-error';
 export { BadRequestError } from './bad-request-error';
+export { NotAuthorizedError } from './not-authorized-error';
--- auth/src/middlewares/index.ts
+++ auth/src/middlewares/index.ts
@@ -1,2 +1,4 @@
 export { errorHandler } from './error-handlers';
 export { validateRequest } from './validate-request';
+export { currentUser } from './curent-user';
+export { requireAuth } from './require-auth';
